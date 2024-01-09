ba394f97872287ba637ff58a13919e47bc500500
luke <48021129@qq.com>
2024-01-09 13:36:37 +0800

    删除注释

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 const argv = process.argv.slice(2);
 // 代理端口
 let port = Number(argv[0]);
-if (isNaN(port)) {
+if (isNaN(port) || port < 1 || port > 65535) {
     port = 5999;
 }
 
@@ -12,7 +12,6 @@ const wss = new WebSocket.Server({ port });
 
 console.log('proxy server start at port: ' + port);
 
-// ws://192.168.1.101:5999/127.0.0.1:8202/wx?name=www&key=C2F6E2A75B9A3468CCE08F276588CC01
 // 当有客户端连接时
 wss.on('connection', (ws, request) => {
     // 连接到被代理的 WebSocket 服务
@@ -30,13 +29,13 @@ wss.on('connection', (ws, request) => {
     // 当接收到客户端的消息时，转发给被代理的 WebSocket 服务
     ws.on('message', function incoming (message) {
         console.log('received: %s', message);
-        targetWs.send(message);
+        targetWs.send(message.toString());
     });
 
     // 当接收到被代理的 WebSocket 服务的消息时，转发给客户端
     targetWs.on('message', function incoming (message) {
         console.log('received: %s', message);
-        ws.send(message);
+        ws.send(message.toString());
     });
 
     // 处理连接关闭事件
